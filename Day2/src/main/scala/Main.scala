adb6b176c899f08654ec5b1824c8018c5f050c3c
Felix Stumvoll <felix.stumvoll@gmail.com>
2020-12-03 12:47:23 +0100

    adds correct day 2 impl

--- Day2/src/main/scala/Main.scala
+++ Day2/src/main/scala/Main.scala
@@ -1,3 +1,30 @@
-object Main extends App {
-  println("fuck you intellij")
+import scala.io.Source
+
+case class LineResult(lowerLimit: Int, upperLimit: Int, character: Char, password: String)
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val inputFile = Source.fromFile("./Day2/input.txt")
+    val lineResults = inputFile
+      .getLines()
+      .map(_.split("[- :]", 4).toList)
+      .flatMap {
+        case lower :: upper :: char :: password :: Nil => Some(LineResult(lower.toInt, upper.toInt, char(0), password))
+        case _ => None
+      }.toList
+    println(part1(lineResults))
+    println(part2(lineResults))
+
+    inputFile.close()
+  }
+
+  def part1(lineResults: Seq[LineResult]): Int =
+    lineResults
+      .count { case LineResult(lower, upper, char, pw) => lower to upper contains pw.count(_ == char) }
+
+  def part2(lineResults: Seq[LineResult]): Int =
+    lineResults
+      .count { case LineResult(lower, upper, char, pw) =>
+        pw.length >= upper && (pw(lower) == char || pw(upper) == char) && (pw(lower) != pw(upper))
+      }
 }
